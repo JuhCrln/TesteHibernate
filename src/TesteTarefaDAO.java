22ba7ddc94abac5acf0c31e6d321765259d173ff
JuhCrln <jullianacrln@gmail.com>
2015-11-28 11:40:06 -0300

    Classe DAO

--- src/TesteTarefaDAO.java
+++ src/TesteTarefaDAO.java
@@ -2,7 +2,17 @@
 public class TesteTarefaDAO {
 	public static void main(String[] args) {
 		TarefaDAO dao = new TarefaDAO();
-		Tarefa tarefa = dao.buscaID(3L);
-		System.out.println(tarefa);
+		Tarefa tarefa = new Tarefa();
+		tarefa = dao.buscaID(5L);
+		//tarefa.setDescricao("Olá");
+		//dao.remove(6L);
+		//Tarefa novatarefa = new Tarefa();
+		//novatarefa.setDescricao("Criando banco com Hibernate");
+		//novatarefa.setFinalizado(true);
+		//novatarefa.setDataFinalizacao(null);
+		//dao.adciona(novatarefa);
+		//novatarefa.setDescricao("Olá");
+		dao.atualiza(tarefa);
+		//System.out.println(tarefa);
 	}
 }
